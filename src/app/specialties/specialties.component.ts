e92e40e32347915e1c1d8c35802718d1265a454e
YahirMaster777 <yahirperez141002@gmail.com>
2024-10-13 02:05:13 -0600

    Creación de estilos normales y de estilos responsivos

---
+++ src/app/specialties/specialties.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialties',
+  standalone: true,
+  imports: [],
+  templateUrl: './specialties.component.html',
+  styleUrl: './specialties.component.css'
+})
+export class SpecialtiesComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { ServicesComponent } from './app/services/services.component';
+import { SpecialtiesComponent } from './app/specialties/specialties.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(ServicesComponent, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(SpecialtiesComponent, appConfig)
   .catch((err) => console.error(err));
